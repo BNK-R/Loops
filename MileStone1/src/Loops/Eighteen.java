1c56a42a9df855a9c76bfe1eb97a40dfba54816a
neelakondareddybeeram <neelakondareddybeeram@192.168.0.4>
2021-05-11 10:05:10 +0530

    First Commit

---
+++ MileStone1/src/Loops/Eighteen.java
@@ -0,0 +1,24 @@
+package Loops;
+import java.util.Scanner;
+public class Eighteen {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int rev=0;
+		int ori=n;
+		while(n > 0)   
+		{  
+		int r = n % 10;  
+		rev =rev * 10 + r;  
+		n = n/10;  
+		}  
+		if(rev==ori)
+		System.out.println("Palindrome");
+		else
+		{
+			System.out.println("Not a Palindrome");
+		}
+
+	}
+
+}
---
+++ MileStone1/src/Loops/Seven.java
@@ -0,0 +1,10 @@
+package Loops;
+import java.util.*;
+public class Seven {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		char c1 = sc.next().charAt(0);
+		System.out.println(Character.toUpperCase(c1));
+	}
+
+}
