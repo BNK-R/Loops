80cc05892c8e4c4b0af66d3e7b72693dd48c8f27
neelakondareddybeeram <neelakondareddybeeram@neelakondas-mbp>
2021-05-12 12:31:16 +0530

    Arrays

---
+++ MileStone3/src/ArrayS/TwentySeven.java
@@ -0,0 +1,22 @@
+package ArrayS;
+import java.util.*;
+
+public class TwentySeven {
+	public static void main(String[] args) {
+		int n,a[],sum=0;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the size of array");
+		n=sc.nextInt();
+		a=new int[n];
+		System.out.println("Enter the array elements");
+		for(int i=0;i<n;i++)
+		{
+			a[i]=sc.nextInt();
+			sum+=a[i];
+		}
+		System.out.println("Sum of array elements is "+sum);
+		sc.close();
+
+	}
+
+}
---
+++ MileStone3/src/ArrayS/Twentyeight.java
@@ -0,0 +1,17 @@
+package ArrayS;
+import java.util.*;
+public class Twentyeight {
+	 public static void main(String[] args) {
+		    Scanner sc = new Scanner(System.in);
+		    System.out.print("Enter number of elements ");
+		    int n = sc.nextInt();
+		    int[] a =new int[n];
+		    int i,j;
+		    for(i=0;i<n;i++) 
+		    {
+		      a[i]=sc.nextInt();
+		    }
+		  }
+
+
+}
