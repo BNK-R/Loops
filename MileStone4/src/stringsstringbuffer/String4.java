7cf0b323801f46419fff8383079a9e275f2ce278
neelakondareddybeeram <neelakondareddybeeram@192.168.0.7>
2021-05-18 12:39:57 +0530

    Strings/Strings Buffer

---
+++ MileStone4/src/stringsstringbuffer/String4.java
@@ -0,0 +1,8 @@
+package stringsstringbuffer;
+public class String4 {
+public static void main(String[] args)
+{
+   String main_string = "Tomcat";    
+   System.out.println(main_string.substring(0, main_string.length()/2));	
+ } 
+}
